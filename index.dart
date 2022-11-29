aecd91d4b6e31f38aa5587ed9710606e6e5406c4
Majesty-ng <113725699+Majesty-ng@users.noreply.github.com>
2022-11-29 13:03:01 +0100

    first dart commit

---
+++ index.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //creating funtion basically creates a syntax
+  ///create and call a function that prints{score of students in maths class}
+  ///it should collect parameter called String(heading)
+  ///stars should be equal to no of char in headers,print the star under the heading
+  //String restart;
+  //do {
+  //restart = "y";
+  //String name;
+  //stdout.write("input the header: ");
+  //name = stdin.readLineSync()!;
+
+  //headingcheck(name);
+
+  //stdout.write("restart?:");
+  // restart = stdin.readLineSync()!;
+  //} while (restart.toLowerCase() != "n");
+//}
+
+//int helpME(int num3, int num4) {
+  //int sum = num3 + num4;
+  //return sum;
+//}
+
+//int Scores(int score1, int score2, int score3, int score4) {
+  //return (score1);
+//}
+
+//int scores(int record) {
+  //return record;
+//}
+
+//String headingcheck(String heading) {
+  //stdout.writeln(heading);
+  //for (int i = 0; i < heading.length; i++) {
+  // if (heading[i] == " ") {
+  //   break;
+  // } else {
+  //   stdout.write("*");
+  // }
+  // if (heading[i] != "") {
+  //   stdout.write("*");
+  // }
+  //stdout.write("*");
+  //}
+  //stdout.writeln();
+  //String done = "done";
+  //return (done);
+
+//String lists
+  List<String> names = ["wesley" "ugwu"];
+  print(names[0]);
+  //mixed list
+  // var mixed = [10, "john", 18.0];
+  // print(mixed[3]);
+  // var list = List<int>.filled(5, 0);
+
+  // print(list[3]);
+  // List<int> numb = [3, 4, 5, 6, 7];
+  // List<int> ages = [3, 43, 56, 16];
+  // ages.addAll(numb);
+  // print(ages);
+}
+
+//function for finding simple interest
+//
