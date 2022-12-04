39191462e905a6cb3fa22ac4132cb0f192c09aa6
Majesty-ng <113725699+Majesty-ng@users.noreply.github.com>
2022-12-04 15:46:26 +0100

    do while to make program reusable

--- assignments/compoundInterest/index.dart
+++ assignments/compoundInterest/index.dart
@@ -28,16 +28,22 @@ class SI {
 }
 
 void main() {
-  print(2);
-  stdout.write("input interest: ");
-  double? principal = double.parse(stdin.readLineSync()!);
-  stdout.write("input rate: ");
-  double? rate = double.parse(stdin.readLineSync()!);
-  stdout.write("input time: ");
-  double? time = double.parse(stdin.readLineSync()!);
-  stdout.write("input number of years: ");
-  double? yr = double.parse(stdin.readLineSync()!);
-  SI simpleinterest = SI();
-  print(simpleinterest);
-  print(simpleinterest.compounInterest(principal, rate, time, yr));
+  //print(2);
+  String restart = "yes";
+  do {
+    stdout.write("input interest: ");
+    double? principal = double.parse(stdin.readLineSync()!);
+    stdout.write("input rate: ");
+    double? rate = double.parse(stdin.readLineSync()!);
+    stdout.write("input time: ");
+    double? time = double.parse(stdin.readLineSync()!);
+    stdout.write("input number of years: ");
+    double? yr = double.parse(stdin.readLineSync()!);
+    SI simpleinterest = SI();
+    //print(simpleinterest);
+    stdout.write("the compound interest is equal to: ");
+    stdout.writeln(simpleinterest.compounInterest(principal, rate, time, yr));
+    stdout.write("do you want to restart? : ");
+    restart = stdin.readLineSync()!;
+  } while (restart == "yes");
 }
